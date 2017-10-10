7ce35f4cf8a50a8c14ad3fd1386aa6db6b4d4b01
abhi_0515 <abhi.for.u55@gmail.com>
2017-10-10 13:47:56 +0530

    added names

--- models/resume-detail.js
+++ models/resume-detail.js
@@ -13,7 +13,21 @@ var Schema = mongoose.Schema;
 	address1: {type:'string', required: true},
 	address2: {type:'string', required: true},
 	email: {type:'string', required: true},
-	phone_no: {type:'number', required: true}
+	phone_no: {type:'number', required: true},
+	employer_name: {type:'string', required: true},
+	industry: {type:'string',required: true},
+	employer_city: {type:'string',required:true},
+	employer_country: {type:'string',required:true},
+	employer_state: {type:'string',required:true},
+	position_title: {type:'string',required:true},
+	start_date: {type:'string',required:true},
+	still_work_here: {type:'boolean',required:true,default:false},
+	school_name: {type:'string',required:true},
+	school_city: {type:'string',required:true},
+	school_state: {type:'string',required:true},
+	school_country: {type:'string',required:true},
+	still_in_school: {type:'boolean',required:true,default:false},
+	additional_skills: {type:'string',required:false}
 });
 
 	module.exports = mongoose.model('ResumeData',resumeDataSchema);
--- routes/index.js
+++ routes/index.js
@@ -29,6 +29,20 @@ router.post('/balloon', function(req, res, next){
 		resumeDetail.address2 = req.body.address2;
 		resumeDetail.email = req.body.email;
 		resumeDetail.phone_no = req.body.phone;
+		resumeDetail.employer_name = req.body.employerName;
+		resumeDetail.industry = req.body.industry;
+		resumeDetail.employer_city = req.body.employerCity;
+		resumeDetail.employer_country = req.body.employerCountry;
+		resumeDetail.employer_state = req.body.employerState;
+		resumeDetail.position_title = req.body.positionTitle;
+		resumeDetail.start_date = req.body.startDate;
+		resumeDetail.still_work_here = req.body.inWork;
+		resumeDetail.school_name = req.body.schoolName;
+		resumeDetail.school_city = req.body.schoolCity;
+		resumeDetail.school_state = req.body.schoolState;
+		resumeDetail.school_country = req.body.schoolCountry;
+		resumeDetail.still_in_school = req.body.inSchool;
+		resumeDetail.additional_skills = req.body.addiSkills;
 
 	resumeDetail.save(function(err){
 		if(err)
